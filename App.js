de3b888827e2572637d900139f37e2b2d0a3079c
JoynalAbedin <jolobangladesh@gmail.com>
2022-10-21 13:59:08 +0600

    crud app done

--- App.js
+++ App.js
@@ -1,20 +1,45 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useReducer } from 'react';
+import TodoList from './src/TodoList';
+import { TodosContext } from './src/TodoContext';
+
+const todosInitialState = {
+    todos: [
+        { id: '1', text: 'Assignment' },
+        { id: '2', text: 'Coding' },
+        { id: '3', text: 'Shopping' },
+    ],
+};
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    const [state, dispatch] = useReducer(todosReducer, todosInitialState);
+    return (
+        <TodosContext.Provider value={{ state, dispatch }}>
+            <TodoList />
+        </TodosContext.Provider>
+    );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+function todosReducer(state, action) {
+    switch (action.type) {
+        case 'add':
+            const addedTodos = [...state.todos, action.payload];
+            return { ...state, todos: addedTodos };
+        case 'edit':
+            const updatedToDo = { ...action.payload };
+            const updatedToDoIndex = state.todos.findIndex(
+                (t) => t.id === action.payload.id
+            );
+            const updatedToDos = [
+                ...state.todos.slice(0, updatedToDoIndex),
+                updatedToDo,
+                ...state.todos.slice(updatedToDoIndex + 1),
+            ];
+            return { ...state, todos: updatedToDos };
+        case 'delete':
+            const filteredTodoState = state.todos.filter(
+                (todo) => todo.id !== action.payload.id
+            );
+            return { ...state, todos: filteredTodoState };
+        default:
+            return todosInitialState;
+    }
+}
---
+++ src/TodoList.js
@@ -0,0 +1,167 @@
+import { View, Text, StyleSheet, TouchableOpacity, Button } from 'react-native';
+import React, { useContext, useState } from 'react';
+import { TodosContext } from './TodoContext';
+import { SwipeListView } from 'react-native-swipe-list-view';
+import {
+    VStack,
+    Input,
+    Icon,
+    NativeBaseProvider,
+    Box,
+    Heading,
+} from 'native-base';
+import uuid from 'uuid-random';
+import { FontAwesome5 } from '@expo/vector-icons';
+const TodoList = () => {
+    const { state, dispatch } = useContext(TodosContext);
+    const [todoText, setTodoText] = useState('');
+    const [editMode, setEditMode] = useState(false);
+    const [editTodo, setEditTodo] = useState(null);
+    const buttonTitle = editMode ? 'Edit' : 'Add';
+
+    const handleSubmit = () => {
+        if (editMode) {
+            dispatch({
+                type: 'edit',
+                payload: { ...editTodo, text: todoText },
+            });
+            setEditMode(false);
+            setEditTodo(null);
+        } else {
+            const newToDo = { id: uuid(), text: todoText };
+            dispatch({ type: 'add', payload: newToDo });
+        }
+        setTodoText('');
+    };
+
+    const renderItem = (data) => (
+        <Box
+            bg={{
+                linearGradient: {
+                    colors: ['blue.500', 'gray.400'],
+                    start: [0, 0],
+                    end: [1, 0],
+                },
+            }}
+            p="12"
+            rounded="xl"
+            _text={{
+                fontSize: 'md',
+                fontWeight: 'bold',
+                color: 'warmGray.50',
+                textAlign: 'center',
+            }}
+        >
+            {data.item.text}
+        </Box>
+    );
+    const renderHiddenItem = (data, rowMap) => (
+        <View style={styles.rowBack}>
+            <TouchableOpacity onPress={() => editRow(data.item, rowMap)}>
+                <Text>Edit</Text>
+            </TouchableOpacity>
+            <TouchableOpacity
+                style={styles.backRightbtn}
+                onPress={() => deleteRow(data.item)}
+            >
+                <Text style={{ color: 'white' }}>Delete</Text>
+            </TouchableOpacity>
+        </View>
+    );
+    const deleteRow = (todo) => {
+        dispatch({ type: 'delete', payload: todo });
+    };
+    const editRow = (todo, rowMap) => {
+        setTodoText(todo.text);
+        setEditMode(true);
+        setEditTodo(todo);
+        if (rowMap[todo.id]) {
+            rowMap[todo.id].closeRow();
+        }
+    };
+    return (
+        <View style={{ flex: 1, marginTop: 60 }}>
+            <View
+                style={{
+                    marginLeft: 5,
+                    marginBottom: 10,
+                }}
+            >
+                <VStack w="100%" space={5} alignSelf="center">
+                    <Heading fontSize="2xl">CRUD</Heading>
+                    <View
+                        style={{
+                            flexDirection: 'row',
+                            marginRight: 60,
+                            marginLeft: 2,
+                        }}
+                    >
+                        <Input
+                            placeholder="Enter To-Do"
+                            value={todoText}
+                            onChangeText={(text) => setTodoText(text)}
+                            variant="filled"
+                            width="100%"
+                            borderRadius="10"
+                            py="1"
+                            px="2"
+                            borderWidth="7"
+                            InputLeftElement={
+                                <Icon
+                                    ml="2"
+                                    size="7"
+                                    color="gray.100"
+                                    as={<FontAwesome5 name="pencil-alt" />}
+                                />
+                            }
+                        />
+                        <Button
+                            onPress={handleSubmit}
+                            title={buttonTitle}
+                            style={{ height: 20 }}
+                        />
+                    </View>
+                </VStack>
+            </View>
+            <SwipeListView
+                data={state.todos}
+                renderItem={renderItem}
+                renderHiddenItem={renderHiddenItem}
+                leftOpenValue={75}
+                rightOpenValue={-75}
+            />
+        </View>
+    );
+};
+const config = {
+    dependencies: {
+        'linear-gradient': require('expo-linear-gradient').LinearGradient,
+    },
+};
+export default () => {
+    return (
+        <NativeBaseProvider config={config}>
+            <TodoList />
+        </NativeBaseProvider>
+    );
+};
+const styles = StyleSheet.create({
+    rowBack: {
+        alignItems: 'center',
+        backgroundColor: '#DDD',
+        flex: 1,
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        paddingLeft: 15,
+    },
+    backRightbtn: {
+        alignItems: 'center',
+        bottom: 0,
+        justifyContent: 'center',
+        position: 'absolute',
+        top: 0,
+        width: 75,
+        backgroundColor: 'red',
+        right: 0,
+    },
+});
